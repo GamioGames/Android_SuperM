f0caa0f725e861e880f5f7a2d6fdc7329bfc4657
antony999k <lv999k@hotmail.com>
2017-01-02 14:22:43 -0600

    Android Studio Video 1, Ejercicio 1. Activity, Paquetes, Layout

---
+++ S01_Ejercicio_CrearLayout/app/src/main/java/com/ejemplo/android/s01_ejercicio_crearlayout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ejemplo.android.s01_ejercicio_crearlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
